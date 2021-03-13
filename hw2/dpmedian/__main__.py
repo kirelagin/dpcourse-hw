4610a39a119c317a6e28b5baca2d8e55289b92da
Kirill Elagin <kirelagin@gmail.com>
2021-03-12 19:29:14 -0500

    hw2: init

---
+++ hw2/dpmedian/__main__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2021 Kirill Elagin <https://kir.elagin.me/>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+def main():
+  print("hello")
---
+++ hw2/flake.nix
@@ -0,0 +1,45 @@
+# SPDX-FileCopyrightText: 2021 Kirill Elagin <https://kir.elagin.me/>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry.url = "github:nix-community/poetry2nix";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, poetry }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ poetry.overlay ]; };
+        inherit (pkgs) poetry2nix;
+
+        src = ./.;
+        app = poetry2nix.mkPoetryApplication {
+          projectDir = src;
+        };
+
+      in {
+        defaultApp = {
+          type = "app";
+          program = "${app}/bin/median-stats";
+        };
+
+        checks = {
+          mypy = pkgs.runCommand "check-mypy" {
+            nativeBuildInputs = [ app.dependencyEnv ];
+          } ''
+          cd "${src}"
+          mypy . tee "$out"
+          '';
+
+          reuse = pkgs.runCommand "check-reuse" {
+            nativeBuildInputs = [ pkgs.reuse ];
+          } ''
+          cd "${src}"
+          reuse lint | tee "$out"
+          '';
+        };
+      }
+    );
+}
